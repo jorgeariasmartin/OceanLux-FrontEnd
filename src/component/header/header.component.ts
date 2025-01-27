216caf1bd206688f0a4ba8a8e9a595ea3780022e
Jorge Arias <jariasmartin@safareyes.es>
2025-01-27 20:44:08 +0100

    ♻️ Update header component

--- src/component/header/header.component.ts
+++ src/component/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
-  templateUrl: './header.component.html'
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
