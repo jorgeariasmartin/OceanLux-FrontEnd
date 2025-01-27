57e3a153da3beaff9f8e4ea8df49c8af0b1ead2e
Jorge Arias <jariasmartin@safareyes.es>
2025-01-27 20:40:33 +0100

    ✨ Add view all trips component

---
+++ src/component/view-all-trips/view-all-trips.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-trips',
+  imports: [],
+  templateUrl: './view-all-trips.component.html'
+})
+export class ViewAllTripsComponent {
+
+}
