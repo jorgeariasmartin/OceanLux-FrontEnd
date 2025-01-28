82bec342e6034cb6d3d70df505b19cd553d4b2fe
Jorge Arias <jariasmartin@safareyes.es>
2025-01-28 20:29:50 +0100

    ♻️ Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import {ViewAllTripsComponent} from '../component/view-all-trips/view-all-trips.component';
+import {ViewAllTripsComponent} from './features/view-all-trips/view-all-trips.component';
 import {HomeComponent} from './features/home/home.component';
+import {ViewLoginRegisterComponent} from './features/view-login-register/view-login-register.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   { path: 'trips', component: ViewAllTripsComponent },
+  { path: 'logaccount', component: ViewLoginRegisterComponent },
 ];
