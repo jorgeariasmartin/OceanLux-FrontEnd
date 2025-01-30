35639258a2efb0d72dd9e2971a513dd0e54106ea
Jorge Arias <jariasmartin@safareyes.es>
2025-01-30 16:26:59 +0100

    ♻️ Update login and register view

--- src/app/features/view-login-register/view-login-register.component.ts
+++ src/app/features/view-login-register/view-login-register.component.ts
@@ -4,11 +4,15 @@ import { SidebarComponent } from '../../../component/sidebar/sidebar.component';
 import { FormsModule } from '@angular/forms';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import {NgIf} from '@angular/common';
+import {Password} from 'primeng/password';
+import {Button} from 'primeng/button';
+import {InputText} from 'primeng/inputtext';
+import { InputNumber } from 'primeng/inputnumber';
 
 @Component({
   selector: 'app-view-login-register',
   standalone: true,
-  imports: [RouterModule, SidebarComponent, FormsModule, SelectButtonModule, NgIf],
+  imports: [RouterModule, SidebarComponent, FormsModule, SelectButtonModule, NgIf, Password, Button, InputText, InputNumber],
   templateUrl: './view-login-register.component.html',
 })
 export class ViewLoginRegisterComponent {
