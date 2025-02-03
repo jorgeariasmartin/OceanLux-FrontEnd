b5c7d36c31133ac761c202f94407bb33b46d99c8
Jorge Arias <jariasmartin@safareyes.es>
2025-02-03 09:47:51 +0100

    ♻️ Update auth view

--- src/app/features/view-login-register/view-login-register.component.ts
+++ src/app/features/view-login-register/view-login-register.component.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { SidebarComponent } from '../../../component/sidebar/sidebar.component';
 import { FormsModule } from '@angular/forms';
 import { SelectButtonModule } from 'primeng/selectbutton';
-import {NgIf} from '@angular/common';
-import {Password} from 'primeng/password';
-import {Button} from 'primeng/button';
-import {InputText} from 'primeng/inputtext';
+import { NgIf } from '@angular/common';
+import { Password } from 'primeng/password';
+import { Button } from 'primeng/button';
+import { InputText } from 'primeng/inputtext';
 import { InputNumber } from 'primeng/inputnumber';
-import { OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { User} from '../../model/User';
 
 @Component({
   selector: 'app-view-login-register',
   standalone: true,
-  imports: [RouterModule, SidebarComponent, FormsModule, SelectButtonModule, NgIf, Password, Button, InputText, InputNumber],
+  imports: [
+    RouterModule,
+    SidebarComponent,
+    FormsModule,
+    SelectButtonModule,
+    NgIf,
+    Password,
+    Button,
+    InputText,
+    InputNumber,
+    HttpClientModule
+  ],
   templateUrl: './view-login-register.component.html',
 })
 export class ViewLoginRegisterComponent implements OnInit {
@@ -23,9 +36,29 @@ export class ViewLoginRegisterComponent implements OnInit {
     { label: 'Registrarse', value: 'register' },
   ];
 
-  activeForm: string = '';
+  activeForm: string = 'login';
+  user: User = { username: '', password: '' }; // Usamos la interfaz User
+  errorMessage: string = '';
 
-  ngOnInit() {
-    this.activeForm = 'login';
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
+
+  login() {
+    if (!this.user.username || !this.user.password) {
+      this.errorMessage = 'Por favor, completa todos los campos.';
+      return;
+    }
+
+    this.authService.login(this.user).subscribe({
+      next: (response) => {
+        console.log('Login exitoso:', response);
+        this.router.navigate(['/']); // Redirigir a la página de inicio
+      },
+      error: (error) => {
+        console.error('Error en login:', error);
+        this.errorMessage = 'Usuario o contraseña incorrectos.';
+      }
+    });
   }
 }
