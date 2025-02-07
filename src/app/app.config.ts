9f628848f59198dfd96f867c41c7a131653dc312
rauld <rcruzadodelgado@safareyes.es>
2025-02-07 16:38:14 +0100

    ⚡config loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,18 +3,23 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import { provideHttpClient } from '@angular/common/http';
-
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { routes } from './app.routes';
+import {customtheme} from './mytheme';
+import {loadingInterceptor} from './Interceptors/loading.interceptor';
+import {authInterceptor} from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(
+      withInterceptors([authInterceptor, loadingInterceptor])
+    ),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: customtheme,
         options: {
           darkModeSelector: false || 'none'
         }
