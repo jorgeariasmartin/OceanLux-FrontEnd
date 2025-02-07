e77607fc482a5a465d45115576ccf30c8cf80aa0
rauld <rcruzadodelgado@safareyes.es>
2025-02-07 16:37:56 +0100

    ⚡️create loading service, interceptor and component

---
+++ src/app/Interceptors/loading.interceptor.ts
@@ -0,0 +1,20 @@
+import {HttpContextToken, HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {finalize} from 'rxjs';
+import {LoadingService} from '../services/loading.service';
+
+export const SkipLoading = new HttpContextToken<boolean>(() => false);
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  const loadingService = inject(LoadingService);
+
+  if (req.context.get(SkipLoading)) {
+    return next(req);
+  }
+
+  loadingService.loadingOn();
+
+  return next(req).pipe(
+    finalize(() => setTimeout(() => loadingService.loadingOff(), 250))
+  );
+};
