dd790289487f34f34c03839ed4169e994dc91ecb
Jorge Arias <jariasmartin@safareyes.es>
2025-02-07 16:45:37 +0100

    ✨ Add route to view trip component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from './features/home/home.component';
 import {ViewLoginRegisterComponent} from './features/view-login-register/view-login-register.component';
 import {MainPageComponent} from './features/main-page/main-page.component';
 import {SearchPageComponent} from './features/search-page/search-page.component';
+import {ViewTripComponent} from './features/view-trip/view-trip.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: MainPageComponent},
   {path: 'search', component: SearchPageComponent},
   {path: 'logaccount', component: ViewLoginRegisterComponent },
+  {path: 'trip/1', component: ViewTripComponent},
 ];
