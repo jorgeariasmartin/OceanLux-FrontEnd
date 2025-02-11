676e9ebf7bfad8f6a55603ca75b91f7647d86c53
Miguel Rico <mricofernandez@safareyes.es>
2025-02-11 23:07:12 +0100

    ✨ Connect this page with backend

--- src/app/features/main-page/main-page.component.ts
+++ src/app/features/main-page/main-page.component.ts
@@ -2,16 +2,30 @@ import { Component } from '@angular/core';
 import {HeaderComponent} from '../../../component/header/header.component';
 import {SearchbarComponent} from '../../../component/searchbar/searchbar.component';
 import {CardComponent} from '../../../component/card/card.component';
+import {Trip} from '../../model/trip';
+import {TripService} from '../../services/trip.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   imports: [
     HeaderComponent,
     SearchbarComponent,
-    CardComponent
+    CardComponent,
+    NgForOf
   ],
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
+  trips: Trip[] = [];
 
+  constructor(private tripService: TripService) { }
+
+  ngOnInit() {
+    this.tripService.allTrips().subscribe(trips => this.trips = trips);
+  }
+
+  loadTrips() {
+    this.tripService.allTrips().subscribe(trips => this.trips = trips);
+  }
 }
