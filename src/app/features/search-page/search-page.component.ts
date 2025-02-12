b73514f70c16c7379c38df2f809c875c463b7306
Miguel Rico <mricofernandez@safareyes.es>
2025-02-12 19:13:12 +0100

    ⚡️ Improve search results

--- src/app/features/search-page/search-page.component.ts
+++ src/app/features/search-page/search-page.component.ts
@@ -5,7 +5,7 @@ import {CardDetailsComponent} from '../../../component/card-details/card-details
 import {ModalFilterComponent} from '../../../component/modal-filter/modal-filter.component';
 import {Trip} from '../../model/trip';
 import {TripService} from '../../services/trip.service';
-import {NgForOf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-search-page',
@@ -14,20 +14,33 @@ import {NgForOf} from '@angular/common';
     SearchbarComponent,
     CardDetailsComponent,
     ModalFilterComponent,
-    NgForOf
+    NgForOf,
+    NgIf
   ],
   templateUrl: './search-page.component.html'
 })
 export class SearchPageComponent {
   trips: Trip[] = [];
+  allTrips: Trip[] = [];
+  searchQuery: string = '';
 
   constructor(private tripService: TripService) { }
 
   ngOnInit() {
-    this.tripService.allTrips().subscribe(trips => this.trips = trips);
+    this.tripService.allTrips().subscribe(trips => {
+      this.allTrips = trips;
+      this.trips = trips;
+    });
   }
 
-  loadTrips() {
-    this.tripService.allTrips().subscribe(trips => this.trips = trips);
+  onSearch(query: string) {
+    const normalizedQuery = query.trim().toLowerCase();
+    this.searchQuery = query;
+
+    this.trips = this.allTrips.filter(trip =>
+      trip.departure.toLowerCase().includes(normalizedQuery) ||
+      trip.name.toLowerCase().includes(normalizedQuery) ||
+      (trip.yacht?.name?.toLowerCase().includes(normalizedQuery))
+    );
   }
 }
