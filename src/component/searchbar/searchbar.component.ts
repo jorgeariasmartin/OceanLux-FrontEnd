d76eba909337dac1681b64370fbf7ec40508a5b8
Miguel Rico <mricofernandez@safareyes.es>
2025-02-12 19:12:43 +0100

    ✨ Searchbar works!!

--- src/component/searchbar/searchbar.component.ts
+++ src/component/searchbar/searchbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './searchbar.component.html'
 })
 export class SearchbarComponent {
+  @Output() searchEvent = new EventEmitter<string>();
+  searchQuery: string = '';
 
+  search() {
+    if (!this.searchQuery.trim()) return;
+    this.searchEvent.emit(this.searchQuery);
+    this.searchQuery = '';
+  }
 }
