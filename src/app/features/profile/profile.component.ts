b7a49c7100bfe5c2c055a6570486ef475bd7c54d
Jorge Arias <jariasmartin@safareyes.es>
2025-02-13 19:46:46 +0100

    ♻️ Update profile component

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../../../component/header/header.component';
 import {SearchbarComponent} from '../../../component/searchbar/searchbar.component';
+import {ButtonModule} from 'primeng/button';
+import {AuthService} from '../../services/auth.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   imports: [
     HeaderComponent,
     SearchbarComponent,
+    ButtonModule,
+    CommonModule
   ],
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent {
+  user: any;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getAuthenticatedUser().subscribe({
+      next: (data) => {
+        this.user = data;
+      },
+      error: (error) => {
+        console.error('Error obteniendo el usuario', error);
+      }
+    });
+  }
 }
