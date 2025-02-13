6b43f94500e3832cd2b93a88c156c309375bb35d
Jorge Arias <jariasmartin@safareyes.es>
2025-02-13 19:46:06 +0100

    ♻️ Update auth service to get data for profile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -51,4 +51,13 @@ export class AuthService {
     this.authStatus.next(false);
     this.router.navigate(['/logaccount']).then(r => r);
   }
+
+  getAuthenticatedUser(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+
+    return this.http.get<any>(`${this.apiUrl}/user/me`, { headers });
+  }
+
 }
