9bb1c118e49f76490d63caf7cda3f7747b735684
Miguel Rico <mricofernandez@safareyes.es>
2025-02-19 00:58:17 +0100

    🔥 Remove unused import

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../../../component/header/header.component';
-import {SearchbarComponent} from '../../../component/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    HeaderComponent,
-    SearchbarComponent,
+    HeaderComponent
   ],
   templateUrl: './profile.component.html'
 })
