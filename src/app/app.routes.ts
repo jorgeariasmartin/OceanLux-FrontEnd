4fe693fe1e68d492d0223e140267552f0c5936eb
rauld <rcruzadodelgado@safareyes.es>
2025-02-19 16:21:27 +0100

    changes on cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ import {AdminTripsComponent} from './features/admin-trips/admin-trips.component'
 import {MainPageComponent} from './features/main-page/main-page.component';
 import {SearchPageComponent} from './features/search-page/search-page.component';
 import {AdminHomepageComponent} from './features/admin-homepage/admin-homepage.component';
+import {AdminYachtsComponent} from './features/admin-yachts/admin-yachts.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'adminTrips', component: AdminTripsComponent},
+  {path:'adminTrips', component: AdminTripsComponent},
   {path: 'logaccount', component: ViewLoginRegisterComponent},
   {path: 'home', component: MainPageComponent},
   {path: 'search', component: SearchPageComponent},
-  {path: 'adminHome', component: AdminHomepageComponent},
-  ];
+  {path:'adminHome', component: AdminHomepageComponent},
+  {path:'adminYachts', component: AdminYachtsComponent}
+
+];
--- src/component/card-details/card-details.component.ts
+++ src/component/card-details/card-details.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {Trip} from '../../app/model/trip';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-card-details',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './card-details.component.html'
 })
 export class CardDetailsComponent {
