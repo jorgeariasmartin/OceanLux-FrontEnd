e776303a65f867568004b3eda66f89141e13a1f1
rauld <rcruzadodelgado@safareyes.es>
2025-02-19 16:40:38 +0100

    🐛 fix bugs for demo

--- src/component/card-details/card-details.component.ts
+++ src/component/card-details/card-details.component.ts
@@ -6,7 +6,7 @@ import {RouterLink} from '@angular/router';
 @Component({
   selector: 'app-card-details',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
     RouterLink
   ],
   templateUrl: './card-details.component.html'
