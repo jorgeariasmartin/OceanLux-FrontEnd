bd25ecce495dfa4e064e63262e694797c2476022
Jorge Arias <jariasmartin@safareyes.es>
2025-02-27 09:16:44 +0100

    ♻️ Change cart

--- src/component/check-tickets/check-tickets.component.ts
+++ src/component/check-tickets/check-tickets.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {AsyncPipe, NgIf} from '@angular/common';
-import {Observable} from 'rxjs';
-import {AuthService} from '../../app/services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {AsyncPipe, CommonModule, NgIf} from '@angular/common';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../app/services/auth.service';
+import { ReservationService } from '../../app/services/booking.service';
 
 @Component({
   selector: 'app-check-tickets',
   imports: [
     RouterLink,
     NgIf,
-    AsyncPipe
+    AsyncPipe,
+    CommonModule
   ],
   templateUrl: './check-tickets.component.html'
 })
-export class CheckTicketsComponent {
+export class CheckTicketsComponent implements OnInit {
   showCartDropdown = false;
   isLoading = true;
+  isLoggedIn!: Observable<boolean>;
+  pendingReservations: any[] = [];
+  userId!: number | null;
+
+  constructor(private authService: AuthService, private bookingService: ReservationService) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isAuthenticated();
+
+    // Obtener el ID del usuario autenticado
+    this.authService.getUserId().subscribe({
+      next: (id) => {
+        this.userId = id;
+        if (this.userId) {
+          this.loadPendingReservations(); // Cargar reservas pendientes solo si el ID del usuario no es null
+        }
+      },
+      error: () => {
+        console.error('No se pudo obtener el ID del usuario.');
+      }
+    });
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  }
 
   toggleCartDropdown() {
     this.showCartDropdown = !this.showCartDropdown;
@@ -28,14 +56,23 @@ export class CheckTicketsComponent {
     }
   }
 
-  isLoggedIn!: Observable<boolean>; // Estado de autenticación
+  loadPendingReservations() {
+    if (!this.userId) return;
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.isLoggedIn = this.authService.isAuthenticated(); // Obtenemos el estado de autenticación
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 1000);
+    this.bookingService.getPendingReservationsByUser(this.userId).subscribe({
+      next: (reservations) => {
+        this.pendingReservations = reservations;
+        console.log(this.pendingReservations);
+      },
+      error: () => {
+        console.error('Error al cargar las reservas pendientes');
+      }
+    });
   }
+
+
+  getTotalPrice(): number {
+    return this.pendingReservations.reduce((acc, r) => acc + r.total_price, 0);
+  }
+
 }
