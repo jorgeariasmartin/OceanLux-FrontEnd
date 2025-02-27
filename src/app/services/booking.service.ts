8a56d3a2806809a7a6e1b86003ede0c55ffdc1f8
Jorge Arias <jariasmartin@safareyes.es>
2025-02-27 09:15:23 +0100

    ♻️ Create booking controller

---
+++ src/app/services/booking.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReservationService {
+  private apiUrl = 'http://localhost:8000/api/booking';
+
+  constructor(private http: HttpClient) {}
+
+  createReservation(reservation: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post<any>(`${this.apiUrl}/create`, reservation, { headers });
+  }
+
+  getPendingReservationsByUser(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/pending/${userId}`);
+  }
+}
