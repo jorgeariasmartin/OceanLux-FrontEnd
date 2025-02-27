69ff5f2d2ccdc3a15b421554df84302eb7187acd
Jorge Arias <jariasmartin@safareyes.es>
2025-02-27 12:04:25 +0100

    ✨ Create guard for routes

---
+++ src/app/guards/admin-role.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminRoleGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAdmin().pipe(
+      tap(isAdmin => {
+        if (!isAdmin) {
+          this.router.navigate(['/search']);  // Redirige si no tiene rol de admin
+        }
+      })
+    );
+  }
+}
