a5528b7e5145ab57c8f59a8b70f686614a104d18
Miguel Rico <mricofernandez@safareyes.es>
2025-03-03 20:53:23 +0100

    ✨ Update card component

--- src/component/card/card.component.ts
+++ src/component/card/card.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {DatePipe, NgClass, NgIf} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {Trip} from '../../app/model/trip';
 
@@ -7,11 +7,20 @@ import {Trip} from '../../app/model/trip';
   selector: 'app-card',
   imports: [
     NgIf,
-    RouterLink
+    RouterLink,
+    DatePipe,
+    NgClass
   ],
   templateUrl: './card.component.html'
 })
 export class CardComponent {
   @Input() shopped: boolean = false;
   @Input() trip!: Trip;
+
+  isPastDate(dateString: string): boolean {
+    const tripDate = new Date(dateString);
+    const today = new Date();
+    return tripDate < today;
+  }
+
 }
